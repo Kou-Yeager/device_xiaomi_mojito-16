da7f2592da5474c81370409bd63bb3a08553d057
Kou-Yeager <wongical552@gmail.com>
2025-08-23 18:07:47 +0700

    mojito: bring up lunaris

--- lineage_mojito.mk
+++ lineage_mojito.mk
@@ -12,20 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
 # Inherit some LineageOS stuffs
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Some Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_BLUR := true
-
-# Whether you are compiling being an OFFICIAL Maintainer:
-INFINITY_BUILD_TYPE := OFFICIAL
-
-# Maintainer Name
-INFINITY_MAINTAINER := K4LCHAKRA_&_ROMGHARTI
+WITH_GMS := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := infinity_mojito
+PRODUCT_NAME := lineage_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
